c1add6f811b871a5b8518f5b3e9913e86db023ea
farizsofyan <topi_jrami@yahoo.com>
2016-09-16 17:19:42 +0700

    Update IIFE js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,18 +1,22 @@
-Macy.init({
-	container: '#macy-container',
-	waitForImages: false,
-	columns: 2,
-	margin: 30,
-});
+(function() {
+	Macy.init({
+		container: '#macy-container',
+		waitForImages: false,
+		columns: 2,
+		margin: 30,
+	});
 
-videojs(document.getElementsByClassName('grid-post-video')[0], {
-	"controls": true,
-	"autoplay": false,
-	"controlBar": {
-		"VolumeControl": false,
-		"fullscreenToggle": false
-	}
-}, function() {
-	// You can grab an element by class if you'd like, just make sure
-	// if it's an array that you pick one (here we chose the first)
-});
+	videojs(document.getElementsByClassName('grid-post-video')[0], {
+		"controls": true,
+		"autoplay": false,
+		"controlBar": {
+			"VolumeControl": false,
+			"fullscreenToggle": false
+		}
+	}, function() {
+		// You can grab an element by class if you'd like, just make sure
+		// if it's an array that you pick one (here we chose the first)
+	});
+
+	
+})();
